7d470379fa229cbd48607d3f773b66ff8a4ffec0
Richard Barrell <rchrd@brrll.co.uk>
2012-02-15 18:59:22 +0000

    Created a setup.py so that depixel can be installed as an egg, which lets it play nice with zc.buildout, easy_install or pip.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages, Extension
+import sys, os
+
+version = '1.0'
+
+setup(name='depixel',
+      version=version,
+      description='Depixeling tool, based on "Depixelizing Pixel Art"',
+      long_description=open('README','rb').read(),
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=["Topic :: Multimedia :: Graphics"],
+      author='Jeremy Thurgood',
+      author_email='firxen@gmail.com',
+      url='https://github.com/jerith/depixel',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'networkx',
+          'pypng',
+          'svgwrite',
+      ],
+      entry_points="""
+      [console_scripts]
+      depixel_png = depixel.scripts.depixel_png:main
+      """,
+      )
